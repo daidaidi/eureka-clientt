61d5324a70d68fdafd332c1da871a2a74adfdefb
lijian <lijian@66law.cn>
2018-01-30 15:42:55 +0800

    eureka-client

---
+++ src/main/java/com/lijian/cn/eurekaclient/controller/DcController.java
@@ -0,0 +1,26 @@
+package com.lijian.cn.eurekaclient.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lijian
+ * @create 2018-01-30 15:25
+ **/
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @GetMapping("/dc")
+    public String dc()
+    {
+
+        String services="Services:"+discoveryClient.getServices();
+        System.out.println(services);
+        return  services;
+    }
+}
